1992ae6278ff81d2270412e0ffe298fd3a764e96
caua <amaralcaua66@gmail.com>
2026-04-28 14:45:06 -0300

    def

--- funçoes_aula2.py
+++ funçoes_aula2.py
@@ -1,1 +1,10 @@
-print ("Ola mundo")
+def formatar_real_replace(valor):
+    texto = f"R$ {valor:..,.2f}"
+    texto = texto.replace(",","X")
+    texto = texto.replace(".",",")
+    texto = texto.replace("X",".")
+    return texto
+
+
+preco =  float(input("digite um numero"))
+print(formatar_real_replace(preco))
